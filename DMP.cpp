59a79c23334cfa87729ec584febcdb76348dae6e
ian910319 <ianjiang319@gmail.com>
2022-06-13 02:02:05 +0800

    add lef

--- DMP.cpp
+++ DMP.cpp
@@ -1,16 +1,33 @@
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <iomanip>
 using namespace std;
+
+class Pin {
+public:
+    char pinName[128]; // Pin name
+    double pinx;          // Pin location
+    double piny;
+    Pin(char _name[128], double  _x, double _y){
+        strcpy(pinName, _name);
+        pinx = _x;
+        piny = _y;
+    }
+};
 
 class Macro {
 public:
-    char name[2048];
-    char type[2048];
+    char name[128];
+    char type[128];
     int x;
     int y;
-    char dir[2048];
-    Macro(char _name[2048], char _type[2048], int _x, int _y, char _dir[2048]){
+    double size_x;
+    double size_y;
+    char dir[128];
+    vector<Pin> PinList;
+
+    Macro(char _name[128], char _type[128], int _x, int _y, char _dir[128]){
         strcpy(name, _name);
         strcpy(type, _type);
         x = _x;
@@ -26,7 +43,7 @@ void myDEFParse(char* F){
 
     const char* comp = "COMPONENTS\0";
     int comp_num;
-    char s[2048];
+    char s[128];
     while(fscanf(DEF_file, "%s", s) != EOF) { 
         if(strcmp(s, comp)==0){
             fscanf(DEF_file, "%s", s);
@@ -39,10 +56,10 @@ void myDEFParse(char* F){
     for(int i=0;i<comp_num;i++){
         fscanf(DEF_file, "%s", s); // "-"
         fscanf(DEF_file, "%s", s); // "name"
-        char name[2048];
+        char name[128];
         strcpy(name, s);
         fscanf(DEF_file, "%s", s); // "type"
-        char type[2048];
+        char type[128];
         strcpy(type, s);
         fscanf(DEF_file, "%s", s); // "+"
         fscanf(DEF_file, "%s", s); // "PLACED/FIXED"
@@ -55,25 +72,65 @@ void myDEFParse(char* F){
         y = atoi(s);
         fscanf(DEF_file, "%s", s); // ")"
         fscanf(DEF_file, "%s", s); // "DIRECTION"
-        char dir[2048];
+        char dir[128];
         strcpy(dir, s);
         fscanf(DEF_file, "%s", s); // ";"
         Macro new_macro(name, type, x, y, dir);
         MacroList.push_back(new_macro);
-        printf("%s %s %d %d %s\n", new_macro.name, new_macro.type, new_macro.x, new_macro.y, new_macro.dir);
+        //printf("%s %s %d %d %s\n", new_macro.name, new_macro.type, new_macro.x, new_macro.y, new_macro.dir);
     }
-    
     fclose(DEF_file);
+};
+
+void myLEFParse(char* F){
+    FILE *LEF_file = fopen(F, "r"); 
+
+    for(int i=0;i<MacroList.size();i++){
+        char macroType[128];
+        strcpy(macroType, MacroList[i].type);
+        char s[128];
+        while(fscanf(LEF_file, "%s", s) != EOF) { 
+            if(strcmp(s, macroType)==0){
+                for(int j=0;j<14;j++) fscanf(LEF_file, "%s", s); //
+                double x = atof(s);
+                fscanf(LEF_file, "%s", s);  //BY
+                fscanf(LEF_file, "%s", s);
+                double y = atof(s);
+                for(int j=0;j<6;j++) fscanf(LEF_file, "%s", s);
+                break;
+            }
+        }
+        while(strcmp(s, "OBS\0")!=0){
+            fscanf(LEF_file, "%s", s); // Now s is pin name
+            char name[128];
+            strcpy(name, s);
+            for(int j=0;j<12;j++) fscanf(LEF_file, "%s", s);
+            double x1 = atof(s);
+            fscanf(LEF_file, "%s", s);
+            double y1 = atof(s);
+            fscanf(LEF_file, "%s", s);
+            double x2 = atof(s);
+            fscanf(LEF_file, "%s", s);  // Now the last number
+            double y2 = atof(s);
+            double x = (x1+x2)/2;
+            double y = (y1+y2)/2;
+            Pin new_pin(name, x, y);
+            MacroList[i].PinList.push_back(new_pin);
+            for(int j=0;j<5;j++) fscanf(LEF_file, "%s", s);
+        }
+
+        rewind(LEF_file);
+    }
+
+    fclose(LEF_file);
 };
 
 int main(int argc, char *argv[]) {
     
     // Parse DEF file
     myDEFParse(argv[1]);
-    cout << endl << MacroList.size() << endl;
     
+    myLEFParse(argv[2]);
+
     return 0;
 }
-
-
-// strcmp(s, obj)==0
